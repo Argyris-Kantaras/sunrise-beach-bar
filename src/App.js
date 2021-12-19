7b37cab2ced118acb0974106d8692c2274d6b013
unknown <argyriskantas@gmail.com>
2021-12-20 00:31:00 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import ContactUs from "./pages/ContactUs";
 import Events from "./pages/Events";
 import Homepage from "./pages/Homepage";
 import classes from "./App.module.css";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className={classes.main}>
-      <Routes>
-        <Route path="/" element={<Homepage />}></Route>
-        <Route path="/events" element={<Events />}></Route>
-        <Route path="/contact-us" element={<ContactUs />}></Route>
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/events" element={<Events />}></Route>
+          <Route path="/contact-us" element={<ContactUs />}></Route>
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
